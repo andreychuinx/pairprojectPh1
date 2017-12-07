62978876845a665b296605ced4d59a3bcf817234
andrey <andrey.amrulloh@gmail.com>
2017-12-07 21:38:11 +0700

    bug pas masukin ke git

--- models/index.js
+++ models/index.js
@@ -4,13 +4,10 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
+
 var env       = 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
-
-
-
-
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
